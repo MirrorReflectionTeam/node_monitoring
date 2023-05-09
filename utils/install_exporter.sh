300fb75e31d6081263d1c8f12e1135f2db446d5e
chabanyknikita <nikchabanyk@gmail.com>
2023-05-09 12:35:08 +0300

    fix alert

--- utils/install_exporter.sh
+++ utils/install_exporter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e "\033[0;32m. Installing node-exporter... \e[0m" && sleep 1
+echo -e "\033[0;32m Installing node-exporter... \e[0m" && sleep 1
 # install node-exporter
 wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
 tar xvfz node_exporter-*.*-amd64.tar.gz
